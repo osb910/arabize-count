9c25af01f1a3eb64b81e98d53e0333aaece1bae4
Omar Shareef Abdul-Bar <omarshdev@gmail.com>
2022-08-23 15:08:52 +0200

    Organizing diacritics & commenting the code

--- arabizeCount.js
+++ arabizeCount.js
@@ -1,25 +1,45 @@
+/**
+ * STATIC VARIABLES AND HELPER FUNCTIONS
+ */
+
+// الحركات والتشكيل
+const diacs = {
+  f: '\u064E', // فتح
+  d: '\u064F', // ضم
+  k: '\u0650', // كسر
+  s: '\u0652', // سكون
+  fn: '\u064B', // تنوين فتح
+  dn: '\u064C', // تنوين ضم
+  kn: '\u064D', // تنوين كسر
+  sh: '\u0651', // شدة
+};
+
 const removeParsingSign = w => w.replace(/[\u064B-\u0650]$/, ''); // حذف علامة الإعراب
-const trimEndingYeh = w => w.replace(/\u064A$/g, '\u064D'); // حذف ياء الاسم المنقوص
+const trimEndingYeh = w => w.replace(/\u064A$/g, diacs.kn); // حذف ياء الاسم المنقوص
 const nominativeToAccusativePlr = w =>
   w.replace(/\u0648\u0646$/, '\u064A\u0646'); // نصب جمع المذكر السالم (أو جره)
 const sign = (w, sign) =>
-  w.replace(/([ء-ي\u0651])[\u064B-\u0650]?$/, `$1${sign}`);
+  w.replace(/([ء-ي\u0651])[\u064B-\u0650]?$/, `$1${sign}`); // وضع علامة الإعراب
 
+/**
+ * MAIN FUNCTION
+ */
 const arabizeCount = (
   num,
   {
-    sng = 'مرة',
-    pair = 'مرتين',
-    plr = 'مرات',
-    zero = 'صفر',
-    isSngVaried = true,
-    isPlrVaried = true,
-    parsing = null,
-    decimal = 0,
-    before = '',
-    after = '',
-    dir = 'rtl',
-    // sngType = 'm', // for written numerals (TODO)
+    sng = 'مرة', // صيغة المفرد
+    pair = 'مرتين', // صيغة المثنى
+    plr = 'مرات', // صيغة الجمع
+    zero = 'صفر', // صيغة الناتج إن كان العدد صفرًا
+    isSngVaried = true, // هل المفرد مصروف
+    isPlrVaried = true, // هل الجمع مصروف
+    parsing = null, // حالة الإعراب
+    keepSign = true, // وضع علامة الإعراب
+    decimal = 0, // عدد الكسور المطلوبة
+    before = '', // عبارة سابقة
+    after = '', // عبارة لاحقة
+    dir = 'rtl', // اتجاه قراءة العدد (أثره غير ملحوظ حتى الآن إلا قليلًا)
+    // sngType = 'm', // for written numerals (TODO) // المعدود مذكر أم مؤنث
   } = {}
 ) => {
   // Numeral formatting | ضبط العدد
@@ -33,6 +53,7 @@ const arabizeCount = (
     int = int >= 0 && int <= 2 && idx ? 100 + int : int;
   }
 
+  // استعمال الأرقام المشرقية وضبط الكسور
   const formattedNum = num.toLocaleString(
     'ar-EG',
     decimal && {
@@ -43,6 +64,7 @@ const arabizeCount = (
 
   // Distinctive formatting | ضبط التمييز
 
+  // إزالة الإعراب إن وُجد
   sng = removeParsingSign(sng);
   plr = removeParsingSign(plr);
 
@@ -53,8 +75,8 @@ const arabizeCount = (
   const accusativeSign = /[\u0627\u0649]$/.test(sng)
     ? ''
     : isSngVaried
-    ? '\u064B' // تنوين نصب
-    : '\u064E'; // فتحة للممنوع من الصرف
+    ? diacs.fn // تنوين نصب
+    : diacs.f; // فتحة للممنوع من الصرف
 
   // ألف تنوين النصب
   const nunationAlif =
@@ -64,19 +86,26 @@ const arabizeCount = (
   const sngGenitiveSign = /[\u0627\u0649]$/.test(sng)
     ? ''
     : isSngVaried
-    ? '\u064D' // تنوين جر
-    : '\u064E'; // فتحة للممنوع من الصرف
+    ? diacs.kn // تنوين جر
+    : diacs.f; // فتحة للممنوع من الصرف
 
   // علامة جر الجمع
   const plrGenitiveSign = /([\u0627\u0649]|\u064A\u0646)$/.test(plrDistinctive)
     ? ''
     : isPlrVaried
-    ? '\u064D' // تنوين جر
-    : '\u064E'; // فتحة للممنوع من الصرف
+    ? diacs.kn // تنوين جر
+    : diacs.f; // فتحة للممنوع من الصرف
 
-  const plrGenitiveDistinctive = sign(plrDistinctive, plrGenitiveSign); // تمييز جمع مجرور
-  const accusativeDistinctive = sign(sng, accusativeSign) + nunationAlif; // تمييز (مفرد) منصوب
-  const sngGenitiveDistinctive = sign(sngDistinctive, sngGenitiveSign); // تمييز مفرد مجرور
+  const plrGenitiveDistinctive = sign(
+    plrDistinctive,
+    keepSign ? plrGenitiveSign : ''
+  ); // تمييز جمع مجرور
+  const accusativeDistinctive =
+    sign(sng, keepSign ? accusativeSign : '') + nunationAlif; // تمييز (مفرد) منصوب
+  const sngGenitiveDistinctive = sign(
+    sngDistinctive,
+    keepSign ? sngGenitiveSign : ''
+  ); // تمييز مفرد مجرور
 
   const zeroResult = num === 0 ? zero : `${formattedNum} ${sngDistinctive}`;
 
@@ -91,7 +120,7 @@ const arabizeCount = (
     parsing === 'nominative'
       ? pair.replace(/[يا]ن$/, 'ان')
       : parsing === 'accusative' || parsing === 'genitive'
-      ? pair.replace(/[يا]ن$/, '\u064E' + 'ين')
+      ? pair.replace(/[يا]ن$/, diacs.f + 'ين')
       : pair;
 
   const arabicCount =
